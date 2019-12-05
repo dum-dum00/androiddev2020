bb49c57c5ffbed0fe54cc9b503edd28b276d28ca
dum-dum00 <deepindark99@gmail.com>
2019-12-06 06:18:39 +0700

    labwork 11 done time to sleep

--- USTH Weather/app/src/main/java/vn/edu/usth/weather/WeatherActivity.java
+++ USTH Weather/app/src/main/java/vn/edu/usth/weather/WeatherActivity.java
@@ -4,7 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentActivity;
 
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.os.Environment;
 import android.util.Log;
 
 import androidx.viewpager.widget.PagerAdapter;
@@ -13,9 +15,16 @@ import com.google.android.material.tabs.TabLayout;
 import androidx.appcompat.app.AppCompatActivity;
 import android.widget.TableLayout;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
 public class WeatherActivity extends FragmentActivity {
 
-
+    MediaPlayer mediaPlayer;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,6 +39,30 @@ public class WeatherActivity extends FragmentActivity {
         TabLayout tabLayout = (TabLayout) findViewById(R.id.tab);
         tabLayout.setupWithViewPager(viewPager);
 
+        InputStream inputStream = getResources().openRawResource(R.raw.wiimusic);
+
+        writeExt();
+        mediaPlayer = MediaPlayer.create(getApplicationContext(), R.raw.wiimusic);
+        mediaPlayer.start();
+    }
+
+    private  void writeExt(){
+        String fname = "wii-music.mp3";
+        String fpath = Environment.getExternalStorageDirectory().getAbsolutePath()+"/Android/data";
+        File file = new File(fpath + fname);
+        try {
+            OutputStream outputStream;
+            InputStream inputStream =getApplicationContext().getResources().openRawResource(R.raw.wiimusic);
+            file.createNewFile();
+            outputStream = new FileOutputStream(file);
+            outputStream.write(inputStream.read());
+            outputStream.close();
+            inputStream.close();
+        }catch (FileNotFoundException ex){
+            ex.printStackTrace();
+        }catch (IOException ex){
+            ex.printStackTrace();
+        }
     }
 
     @Override
@@ -54,7 +87,8 @@ public class WeatherActivity extends FragmentActivity {
     protected void onPause() {
         super.onPause();
         Log.i("Pause", "This is onPause function");
-
+        mediaPlayer.stop();
+        mediaPlayer.release();
     }
 
     @Override
